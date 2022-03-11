622337356a11ae615e31c280e48f3024c05be717
bryanhjj <83968222+bryanhjj@users.noreply.github.com>
2022-03-11 19:08:21 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+var cors = require("cors");
+var bodyParser = require("body-parser"); // maybe remove?
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var passport = require("passport");
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+app.use(cookieParser());
+app.use(cors({
+    origin: 'https://bryanhjj.github.io',
+    optionsSuccessStatus: 200,
+}));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, "public")));
+
+app.use('/', indexRouter);
+
+module.exports = app;
