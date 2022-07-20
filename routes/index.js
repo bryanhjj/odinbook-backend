aebb8f764f8d94f592590c45643431bfd8a6d63c
Bryan <bryan.how@live.com>
2022-07-20 20:48:39 +0800

    bug fixes

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,9 @@ passport.use(facebookTokenStrategy);
 const authRouter = require('./auth');
 const userRouter = require('./users');
 const postRouter = require('./posts');
-const commentRouter = require('./comments');
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
-router.use('/posts/:postId/comments', commentRouter);
 router.use('/posts', postRouter);
 
 module.exports = router;
