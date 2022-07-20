015ed072698c39587ffae93d5c9c940709e259cc
Bryan <bryan.how@live.com>
2022-07-20 22:05:43 +0800

    bug fixes

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 require("dotenv").config();
+require("./utils/mongoConfig");
 var cors = require("cors");
-var bodyParser = require("body-parser"); // maybe remove?
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var passport = require("passport");
+var session = require("express-session");
 
 var indexRouter = require('./routes/index');
 
 var app = express();
-app.use(cookieParser());
-app.use(cors());
+
+// to update cors accordingly
+app.use(cors({
+    origin: 'https://bryanhjj.github.io',
+    credentials: true,
+}));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(session({secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/public', express.static(path.join(__dirname, "public")));
 
 app.use('/', indexRouter);
+// to show "route not available" if users try to access a route that doesn't exits.
+app.use("*", (req, res) => res.status(404).json({error: "route not available."}));
 
 module.exports = app;
